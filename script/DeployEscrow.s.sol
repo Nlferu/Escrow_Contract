aee7cc29379d68ac907a257a3caa9109747deef1
Niferu <wargacki.patryq@gmail.com>
2024-02-29 17:32:34 +0100

    deploy script implemented

---
+++ script/DeployEscrow.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Escrow} from "../src/Escrow.sol";
+import {console} from "forge-std/Test.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployEscrow is Script {
+    function run() external returns (Escrow) {
+        uint256 deployerKey = vm.envUint("LOCAL_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerKey);
+        Escrow escrow = new Escrow();
+        console.log("Raiser Deployed -> Owner: ", msg.sender);
+        vm.stopBroadcast();
+
+        return (escrow);
+    }
+}
