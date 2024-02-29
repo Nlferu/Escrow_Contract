55fbc0e15eafec31337c3ee481c0075bacd249fc
Niferu <wargacki.patryq@gmail.com>
2024-02-29 17:33:04 +0100

    proper escrow test initial added

---
+++ test/unit/EscrowTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Vm} from "forge-std/Vm.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {DeployEscrow} from "../../script/DeployEscrow.s.sol";
+import {Escrow} from "../../src/Escrow.sol";
+
+contract EscrowTest is StdCheats, Test {}
