7a1e06d5f388b01f3e55a95ca838829c7d449777
Niferu <wargacki.patryq@gmail.com>
2024-03-05 00:53:46 +0100

    script adjusted

--- script/DeployAstaroth.s.sol
+++ script/DeployAstaroth.s.sol
@@ -6,18 +6,18 @@ import {console} from "forge-std/Test.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployAstaroth is Script {
-    function run() external returns (Astaroth) {
+    function run() external returns (Astaroth, address) {
         uint256 deployerKey = vm.envUint("AST_PRIVATE_KEY");
 
         uint256 astSupply = 7000;
 
         vm.startBroadcast(deployerKey);
         Astaroth astaroth = new Astaroth(astSupply);
+        address astOwner = vm.addr(deployerKey);
         console.log("Astaroth Token Deployed: ", address(astaroth));
-        console.log("Astaroth Token Owner: ", vm.addr(deployerKey));
-        console.log(address(this));
+        console.log("Astaroth Token Owner: ", astOwner);
         vm.stopBroadcast();
 
-        return (astaroth);
+        return (astaroth, astOwner);
     }
 }
