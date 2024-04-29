5a3d19d78761ae79c3b06ed1e2ca694a19da1e8d
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-04-29 23:21:47 +0200

    deploy import adjusted

--- script/DeployAstaroth.s.sol
+++ script/DeployAstaroth.s.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {Astaroth} from "../src/tokens/Astaroth.sol";
-import {console} from "forge-std/Test.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DeployAstaroth is Script {
     function run() external returns (Astaroth, address) {
